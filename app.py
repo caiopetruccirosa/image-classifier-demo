9f4fbe4a096e3007e0d6046e97655bfc3a05a995
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2024-09-04 17:16:26 -0300

    translate text to portuguese and change layout to two columns

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from classifier import ImageClassifier
 ##################
 # Set page config
 ##################
-st.set_page_config(page_title="Image Classifier Demo", page_icon="👁️", layout="wide")
+st.set_page_config(page_title="Classificador de Imagens", page_icon="👁️", layout="wide")
 
 ###################
 # Helper functions 
@@ -34,37 +34,78 @@ models = {
 # App GUI
 ##########
 
-st.title("Demo: Image Classification")
+st.title("Demo: Classificação de Imagens")
+
+# placeholder for the header
+header_placeholder = st.empty()
 
 selected_model = st.selectbox(
-    "Select a model for image classification:",
+    "Escolha um modelo para classificar as imagens:",
     models.keys(),
-    placeholder="Select a model..."
+    placeholder="Escolha um modelo..."
 )
 
-if selected_model is not None:
-    image_classifier = load_classifier(models[selected_model])
-    
-    st.header(f"Classifying with {selected_model} ({image_classifier.model.config._name_or_path})")
+if selected_model is not None:    
+    # assign the header title
+    header_placeholder.header(f"Classificando com {selected_model} ({models[selected_model]})")
 
-    upload = st.file_uploader(label="Upload image:", type=["png", "jpg", "jpeg"])
+    image_classifier = load_classifier(models[selected_model])
+
+    upload = st.file_uploader(label="Carregar imagem:", type=["png", "jpg", "jpeg"])
+    css = """
+    <style>
+        div[data-testid="stFileUploader"]>section[data-testid="stFileUploaderDropzone"]>button {{
+            display: none;
+        }}
+        div[data-testid="stFileUploaderDropzoneInstructions"]>div>span {{
+            visibility:hidden;
+            font-size: 0px;
+        }}
+        div[data-testid="stFileUploaderDropzoneInstructions"]>div>span::after {{
+            content:"{INSTRUCTIONS_TEXT}";
+            visibility:visible;
+            display:block;
+            font-size: 16px;
+        }}
+        div[data-testid="stFileUploaderDropzoneInstructions"]>div>small {{
+            visibility:hidden;
+            font-size: 0px;
+        }}
+        div[data-testid="stFileUploaderDropzoneInstructions"]>div>small::before {{
+            content:"{FILE_LIMITS}";
+            visibility: visible;
+            display:block;
+            font-size: 14px;
+        }}
+    </style>
+    """.format(
+        INSTRUCTIONS_TEXT="Arraste e solte a imagem aqui ou clique aqui para carregar.",
+        FILE_LIMITS="Limite de 200MB por arquivo • PNG, JPG, JPEG",
+    )
+    st.markdown(css, unsafe_allow_html=True)
 
     if upload:
-        img = Image.open(upload)
+        img = Image.open(upload).convert("RGB")
 
         scores = image_classifier.predict(img)
         scores_df = pd.DataFrame.from_dict(scores)
+        
+        prediction = scores_df['score'].idxmax()
 
-        top_5 = scores_df.sort_values(by="score", ascending=False).head(5)
-        top_prob = top_5.iloc[0]['score']
+        top_label = scores_df.loc[prediction, 'label']
+        top_prob = scores_df.loc[prediction, 'score']
 
-        fig = px.bar(top_5, x="score", y="label", orientation='h')
+        top_5 = scores_df.sort_values(by="score").tail(5)
+        top_5['score'] = top_5['score'] * 100
 
-        st.subheader(f"Predicted label:")
-        st.markdown(f"**{top_5.iloc[0]['label']}** with {top_prob*100:.2f}% certainty.")
+        fig = px.bar(top_5, x="score", y="label", orientation='h', title="Top 5 classes possíveis")
 
-        st.subheader("Predicted top 5 probabilities:")
-        st.plotly_chart(fig, theme="streamlit")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader(f"Classificação:")
+            st.markdown(f"Classe \"**{top_label}**\" com {top_prob*100:.2f}% de probabilidade.")
+            st.plotly_chart(fig, theme="streamlit")
 
-        st.subheader("Image:")
-        st.image(img, caption=f"Uploaded image: {upload.name}")
+        with col2:
+            st.subheader("Imagem:")
+            st.image(img, caption=f"Uploaded image: {upload.name}")
